b0759fc416ee5afee2210d7cec1b19b0d042792b
Simon Rodgers <simonpeterodgers@gmail.com>
2026-04-09 20:46:08 +1000

    changes

--- init.lua
+++ init.lua
@@ -7,12 +7,10 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 0
 vim.o.expandtab = true
+vim.o.signcolumn = "yes"
 
 -- Hide command line
 vim.o.cmdheight = 0
-
--- Clear highlighting by pressing esc
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 --
 -- Key binds
@@ -26,9 +24,11 @@ require("which-key").setup({
     { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
     { '<leader>t', group = '[T]oggle' },
     { 'gr', group = 'LSP Actions', mode = { 'n' } },
-  }
+  };
+  win = { border = "rounded"; }
 })
 
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 
 vim.keymap.set('n', '<leader>sh', ":Telescope help_tags<CR>", { desc = '[S]earch [H]elp' })
@@ -48,6 +48,7 @@ vim.keymap.set('n', '<leader>td', function()
     virtual_text = not vim.diagnostic.config().virtual_text,
   })
 end, { desc = '[T]oggle [D]iagnostic lines' })
+vim.diagnostic.config({ update_in_insert = true })
 
 require("mini.icons").setup()
 require("mini.indentscope").setup()
@@ -64,18 +65,15 @@ require("blink-cmp").setup({
     documentation = {
       auto_show = true,
       auto_show_delay_ms = 500,
-      window = {
-        border = "rounded"
-      }
-
+      window = { border = "rounded" }
     },
-    signature = {
-      enabled = true,
-    },
+    signature = { enabled = true, },
   }
 })
 
-
+--
+-- Colorscheme
+--
 local function set_colorscheme(background)
   if background == "dark" then
     require("mini.hues").setup({
@@ -92,6 +90,9 @@ local function set_colorscheme(background)
       accent = "blue",
     })
   end
+
+  vim.cmd('hi MiniCursorword gui=nocombine guibg=#504945')
+  vim.cmd('hi MiniCursorwordCurrent gui=nocombine guibg=#504945')
 end
 
 vim.api.nvim_create_autocmd("OptionSet", {
@@ -103,6 +104,9 @@ vim.api.nvim_create_autocmd("OptionSet", {
 
 set_colorscheme(vim.o.background)
 
+--
+-- LSP
+-- 
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("nixd")
